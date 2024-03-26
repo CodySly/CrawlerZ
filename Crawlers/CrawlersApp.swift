8382b43e9e3705884e5146454a09cf99a703e5c7
CodysBurgess <CodysBurgess@gmail.com>
2024-03-26 12:54:11 -0600

    Initial Commit

---
+++ Crawlers/CrawlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrawlersApp.swift
+//  Crawlers
+//
+//  Created by Cody Burgess on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CrawlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
